b727e31adaee3c901f6bda315b8acfb94bd663d7
Sarvesh <65279346+sarveshsuresh@users.noreply.github.com>
2021-09-30 00:10:22 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -24,7 +24,7 @@ from gsheetsdb import connect
 
 # Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["service-acc"],
+    st.secrets["111530319391762022741"],
     scopes=[
         "https://www.googleapis.com/auth/spreadsheets",
     ],
