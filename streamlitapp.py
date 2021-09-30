6ba168e669d194f4f44067ea74be77f6861abe7f
Sarvesh <65279346+sarveshsuresh@users.noreply.github.com>
2021-09-30 13:25:06 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -42,7 +42,7 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-data1=pd.read_csv('complete_backtester_refined.csv')
+data1=pd.read_csv('complete_backtester_refined_head.csv')
 data = pd.DataFrame(rows,columns=list(data1.columns))
 st.write(data.head(3))
 
