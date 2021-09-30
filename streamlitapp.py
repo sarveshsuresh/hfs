2e1c2bdc0c1c5935d9ba6259eaeb3a387fadff31
Sarvesh <65279346+sarveshsuresh@users.noreply.github.com>
2021-09-30 13:43:08 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -35,6 +35,7 @@ conn = connect(credentials=credentials)
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 
+@st.cache(allow_output_mutation=True)
 def run_query(query):
     rows = conn.execute(query, headers=1)
     return rows
